9df9acc895b4036113c915a95d7f81cc87dd995b
Куниловский Евгений Георгиевич <Evgeniy.Kunilovskiy@home.bank>
2024-06-08 16:32:58 +0300

    add Example

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -3,14 +3,18 @@ package com.dmdev.spring;
 import com.dmdev.spring.database.pool.ConnectionPool;
 import com.dmdev.spring.database.repository.CompanyRepository;
 import com.dmdev.spring.database.repository.UserRepository;
+import com.dmdev.spring.ioc.Container;
 import com.dmdev.spring.service.UserService;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        var connectionPool = new ConnectionPool();
-        var userRepository = new UserRepository(connectionPool);
-        var companyRepository = new CompanyRepository(connectionPool);
-        var userService = new UserService(userRepository, companyRepository);
+//        var connectionPool = new ConnectionPool();
+//        var userRepository = new UserRepository(connectionPool);
+//        var companyRepository = new CompanyRepository(connectionPool);
+//        var userService = new UserService(userRepository, companyRepository);
+
+        var container = new Container();
+        var userService = container.get(UserService.class);
         // TODO userService
     }
 }
