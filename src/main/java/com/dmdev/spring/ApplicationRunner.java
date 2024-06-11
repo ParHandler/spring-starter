20e04c93701e882b0859ba433adffc5f2d3af7ac
Куниловский Евгений Георгиевич <Evgeniy.Kunilovskiy@home.bank>
2024-06-11 15:57:03 +0300

    add Example 6. Constructor Injection

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationRunner {
     public static void main(String[] args) {
         var context = new ClassPathXmlApplicationContext("application.xml");
-        var connectionPool = (ConnectionPool) context.getBean("pool2",ConnectionPool.class);
+        var connectionPool = (ConnectionPool) context.getBean("p1",ConnectionPool.class);
         System.out.println(connectionPool);
     }
 }
